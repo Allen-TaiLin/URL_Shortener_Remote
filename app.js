cc09ef7ec84dc029c35eeca349ed8ecdabb84013
Allen-TaiLin <skygary2000@gmail.com>
2020-11-27 01:15:30 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const urlExists = require('url-exists')
 const app = express()
 const port = 3000
 
@@ -17,9 +18,62 @@ const generateOutput = require('./public/generateOutput')
 // 設定資料庫
 require('./config/mongoose')
 
+const Shortener = require('./models/shortUrl')
+// 解析 port 當前的狀態 決定要回傳那個 host
+const host = 'http://localhost:3000/shortener/'
+
+
+
 app.get('/', (req, res) => {
 
   res.render('index')
+})
+
+
+app.post('/', (req, res) => {
+  const checkUrl = req.body.url
+  console.log('this is ' + checkUrl.toUpperCase())
+
+  urlExists(checkUrl, function (err, exists) {
+    //console.log(exists)
+    if (exists) {
+      Shortener.findOne({ hostName: checkUrl })
+        .lean()
+        .then((url) => {
+          // 是否存在
+          if (url) {
+            console.log('找到了匹配資料，渲染畫面！')
+            return res.render('index', { code: url.shortName, host })
+          } else {
+            console.log('沒有找到匹配資料，生成一組code並開始進行後續動作！')
+            let shortName = generateOutput()
+
+            Shortener.find()
+              .then((urls) => {
+                // 檢查 shortName 是否有重複
+                while (urls.find((item) => {
+                  item.shortName === shortName
+                })) {
+                  shortName = generateOutput()
+                }
+
+                Shortener.create({
+                  hostName: checkUrl,
+                  shortName: shortName
+                })
+                  .then(() => {
+                    res.render('index', { code: Shortener, host })
+                  })
+
+              })
+              .catch((error) => console.log(error))
+          }
+
+
+        })
+        .catch((error) => console.log(error))
+    }
+  })
 })
 
 // start and listen on the Express server
