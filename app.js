e684ff211498b2e0019ff81363d541831003714c
Allen-TaiLin <skygary2000@gmail.com>
2020-11-26 23:26:03 +0800

    feat: add body-parser

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+// setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
+const generateOutput = require('./public/generateOutput')
 
 
 // 設定資料庫
@@ -17,6 +22,7 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
