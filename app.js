19c5df23a2011fa0b92e8989ec4926bcccac68dd
Allen-TaiLin <skygary2000@gmail.com>
2020-11-26 21:59:30 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
 
 // 設定資料庫
 require('./config/mongoose')
 
 app.get('/', (req, res) => {
-  res.send('short')
+
+  res.render('index')
 })
 
 app.listen(port, () => {
